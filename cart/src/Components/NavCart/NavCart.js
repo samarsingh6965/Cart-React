ea53d4597c766f51106db06268996859da97ec7b
Samar Raj <samar.suvaidyam@gmail.com>
2023-05-16 15:29:27 +0530

    quantity selector added

--- cart/src/Components/NavCart/NavCart.js
+++ cart/src/Components/NavCart/NavCart.js
@@ -1,31 +1,59 @@
 import { MdDelete } from 'react-icons/md'
 import cartEmpty from '../../Assets/empty.png'
 
-const NavCart = ({ data , setData}) => {
+const NavCart = ({ data, setData }) => {
     const handleRemove = (e) => {
         let index = data.findIndex((el) => el.id === e)
-        data.splice(index,1)
+        data.splice(index, 1)
         setData([...data])
-     }
-    
+    }
+    const handleMinus = (e) => {
+        e.Qty = e.Qty - 1
+        if (e.Qty <= 1) {
+            e.Qty = 1
+        }
+        setData([...data])
+    }
+
+    const handlePlus = (e) => {
+        e.Qty = e.Qty + 1
+        if (e.Qty >= 5) {
+            e.Qty = 5
+        }
+        setData([...data])
+    }
+
     return (
         <>
             {/* cart Bar Started  */}
-            <div className="w-1/3 max-md:w-3/5 max-lg:w-1/2 h-screen fixed z-50 flex gap-2 flex-col border bg-gray-300 top-14 right-0 overflow-y-scroll p-2">
-                {data.length === 0 ? <p className='text-center text-3xl mt-6'><img src={cartEmpty} alt="" /></p>
-                    : data.map(e => (
-                        <div key={e.id} className="w-full bg-white flex flex-wrap justify-between border">
-                            <div className="w-full flex max-sm:flex-col gap-4 items-center my-1 p-2">
-                                <img src={e.img} alt="" className='w-[49%] max-sm:w-full h-[150px] border p-2' />
-                                <div className=" w-[50%] h-[140px] max-sm:w-full max-sm:h-auto leading-10 relative">
-                                    <h1 className=' font-semibold'>{e.name}</h1>
-                                    <p className=" font-semibold">Rs. {e.price}<span className='pl-2 text-green-500 text-xs'>50% off</span></p>
-                                    <MdDelete onClick={() => handleRemove(e.id)} className='text-3xl absolute top-0 right-2 cursor-pointer'/>
-                                    <button className='bg-blue-500 mt-3 rounded-md w-full text-white h-8 leading-8'>Buy</button>
+            <div className="w-1/3 justify-between max-md:w-3/5 max-lg:w-1/2 h-[93vh] fixed z-50 flex gap-2 flex-col border bg-gray-300 top-14 right-0 p-2">
+                <div className='h-[70vh] overflow-y-auto'>
+                    {data.length === 0 ? <p className='text-center text-3xl mt-6'><img src={cartEmpty} alt="" /></p>
+                        : data.map(e => (
+                            <div key={e.id} className="w-full bg-white flex flex-wrap justify-between border">
+                                <div className="w-full flex max-sm:flex-col gap-4 items-center my-1 p-2">
+                                    <img src={e.img} alt="" className='w-[49%] max-sm:w-full h-[150px] border p-2' />
+                                    <div className=" w-[50%] h-[140px] max-sm:w-full max-sm:h-auto leading-10 relative">
+                                        <h1 className=' font-semibold'>{e.name}</h1>
+                                        <p className=" font-semibold">Rs. {e.price}<span className='pl-2 text-green-500 text-xs'>50% off</span></p>
+                                        <MdDelete onClick={() => handleRemove(e.id)} className='text-3xl absolute top-0 right-2 cursor-pointer' />
+                                        <div className="flex items-center gap-1">
+                                            <button onClick={() => handleMinus(e)} className='px-4 text-3xl border rounded-md'>-</button>
+                                            <p className='w-7 h-7 flex items-center justify-center border rounded-full'>{e.Qty}</p>
+                                            <button onClick={() => handlePlus(e)} className='px-4 text-3xl border rounded-md'>+</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                </div>
+                <div className="w-full h-[23vh] bg-orange-400 flex flex-col p-6 justify-between">
+                    <div className="flex flex-col gap-3">
+                        <p className='text-xl max-sm:text-base'>Delivery Charges : <span className='max-sm:text-base w-auto max-sm:py-0 px-2 py-1 text-lg border border-slate-600'>{49}</span></p>
+                        <p className='text-xl max-sm:text-base'>Total : <span className='max-sm:text-base w-auto px-2 max-sm:py-0 py-1 text-lg border border-slate-600'>{120000}</span></p>
+                    </div>
+                    <button className='p-2 max-sm:p-0 bg-white rounded-md'>Checkout</button>
+                </div>
             </div>
             {/* Cart Bar Ended  */}
         </>
--- cart/src/Components/Products/Products.js
+++ cart/src/Components/Products/Products.js
@@ -8,8 +8,8 @@ import product6 from '../../Assets/download (1).jpeg';
 import product7 from '../../Assets//download (2).jpeg';
 import product8 from '../../Assets/download (3).jpeg';
 import product9 from '../../Assets/download (4).jpeg';
-import { AiOutlineShoppingCart, AiOutlineHeart, AiFillHeart,AiFillStar,AiOutlineStar } from 'react-icons/ai';
-import {BsStarHalf} from 'react-icons/bs'
+import { AiOutlineShoppingCart, AiOutlineHeart, AiFillHeart, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { BsStarHalf } from 'react-icons/bs'
 import { useState } from 'react';
 
 
@@ -17,141 +17,220 @@ import { useState } from 'react';
 const ProList = [{
     id: 1,
     img: product1,
-    name: 'Ideapad Laptop',
+    name: 'Ideapad',
     description: 'iusto expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '1,90,000',
+    price: 190000,
+    Qty:1
 },
 {
     id: 2,
     img: product2,
     name: 'Dell Laptop',
     description: 'iusto expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '1,20,000',
+    price: 120000,
+    Qty:1
 },
 {
     id: 3,
     img: product3,
     name: 'HP Laptop',
     description: 'iusto expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '1,20,000',
+    price: 120000,
+    Qty:1
 },
 {
     id: 4,
     img: product4,
     name: 'Macbook',
     description: 'Dicta sunt iusto expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '2,90,000',
+    price: 290000,
+    Qty:1
 },
 {
     id: 5,
     img: product5,
     name: 'Dell Laptop',
     description: 'expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '1,20,000',
+    price: 120000,
+    Qty:1
 },
 {
     id: 6,
     img: product6,
     name: 'HP Laptop',
     description: 'iusto expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '1,20,000',
+    price: 120000,
+    Qty:1
 },
 {
     id: 7,
     img: product7,
     name: 'Macbook',
     description: ' Dicta sunt iusto expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '2,90,000',
+    price: 290000,
+    Qty:1
 },
 {
     id: 8,
     img: product8,
     name: 'HP Laptop',
     description: 'Lorem ipsum impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '1,20,000',
+    price: 120000,
+    Qty:1
 },
 {
     id: 9,
     img: product9,
     name: 'Macbook',
     description: 'Lorem ipsum a impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '2,90,000',
+    price: 290000,
+    Qty:1
 },
 {
     id: 10,
     img: product2,
     name: 'Dell Laptop',
     description: 'Lorem ipsum dolor expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '1,20,000',
+    price: 120000,
+    Qty:1
 },
 {
     id: 11,
     img: product3,
     name: 'HP Laptop',
     description: 'Lorem ipsum dolor sit amet impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '1,20,000',
+    price: 120000,
+    Qty:1
 },
 {
     id: 12,
     img: product4,
     name: 'Macbook',
     description: 'Lorem ipsum expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '2,90,000',
+    price: 290000,
+    Qty:1
 },
 {
     id: 13,
     img: product3,
     name: 'HP Laptop',
     description: 'Lorem ipsum dolor expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '1,20,000',
+    price: 120000,
+    Qty:1
 },
 {
     id: 14,
     img: product4,
     name: 'Macbook',
     description: 'Lorem ipsum dolor expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '2,90,000',
+    price: 290000,
+    Qty:1
 },
 {
     id: 15,
     img: product3,
     name: 'HP Laptop',
     description: 'Lorem ipsum iusto expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '1,20,000',
+    price: 120000,
+    Qty:1
 },
 {
     id: 16,
     img: product3,
     name: 'HP Laptop',
     description: 'Lorem ipsum iusto expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
-    price: '1,20,000',
+    price: 120000,
+    Qty:1
+},
+{
+    id: 17,
+    img: product9,
+    name: 'Macbook',
+    description: 'Lorem ipsum a impedit hic dolorum saepe unde suscipit esse! Deserunt',
+    price: 290000,
+    Qty:1
+},
+{
+    id: 18,
+    img: product2,
+    name: 'Dell Laptop',
+    description: 'Lorem ipsum dolor expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
+    price: 120000,
+    Qty:1
+},
+{
+    id: 19,
+    img: product3,
+    name: 'HP Laptop',
+    description: 'Lorem ipsum dolor sit amet impedit hic dolorum saepe unde suscipit esse! Deserunt',
+    price: 120000,
+    Qty:1
+},
+{
+    id: 20,
+    img: product4,
+    name: 'Macbook',
+    description: 'Lorem ipsum expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
+    price: 290000,
+    Qty:1
+},
+{
+    id: 21,
+    img: product3,
+    name: 'HP Laptop',
+    description: 'Lorem ipsum dolor expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
+    price: 120000,
+    Qty:1
+},
+{
+    id: 22,
+    img: product4,
+    name: 'Macbook',
+    description: 'Lorem ipsum dolor expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
+    price: 290000,
+    Qty:1
+},
+{
+    id: 23,
+    img: product3,
+    name: 'HP Laptop',
+    description: 'Lorem ipsum iusto expedita impedit hic dolorum saepe unde suscipit esse! Deserunt',
+    price: 120000,
+    Qty:1
 }]
 function Products({ setData, data }) {
     const [toggleIcon, setToggleIcon] = useState(ProList)
     const AddToCart = (e) => {
-        setData([...data, e])
-    }
-
-    const toggle = (e) => {
-        e.toggle  = ! e.toggle
-        setToggleIcon([...toggleIcon])
+        let index = data.indexOf(e) === -1
+        if(index){
+            setData([...data, e])
+        }else{
+            alert(e.name + " is already added in Cart.")
+        }
+       
     }
     
+    const toggle = (e) => {
+        e.toggle = !e.toggle
+        setToggleIcon([...toggleIcon])
+    }
+   
+
 
 
     return (
         <>
-            <div className="flex justify-center  flex-wrap gap-5 ">
+            <div className="flex justify-center flex-wrap gap-5">
                 {ProList.map(e => (
                     <div className="w-[300px] min-w-[300px] rounded-md border p-1" key={e.id}>
                         <div className="relative">
                             <img src={e.img} alt="img" className='h-[300px] border p-3' />
-                            <i className='absolute top-2 right-2 cursor-pointer' onClick={()=>toggle(e)}>{e.toggle? <AiFillHeart  className='text-red-500 text-lg' />:<AiOutlineHeart  className='text-red-500 text-lg'/>}</i>
+                            <i className='absolute top-2 right-2 cursor-pointer' onClick={() => toggle(e)}>{e.toggle ? <AiFillHeart className='text-red-500 text-lg' /> : <AiOutlineHeart className='text-red-500 text-lg' />}</i>
                         </div>
                         <div className="px-2 flex flex-col gap-2">
-                            <i className='flex py-2 text-orange-500 text-lg' ><AiFillStar/><AiFillStar/><AiFillStar/><BsStarHalf className='text-base'/><AiOutlineStar/></i>
+                            <i className='flex py-2 text-orange-500 text-lg' ><AiFillStar /><AiFillStar /><AiFillStar /><BsStarHalf className='text-base' /><AiOutlineStar /></i>
                             <p className='text-2xl font-serif'>{e.name}</p>
-                            <p className='leading-6 h-12 overflow-y-clip'>{e.description}</p>
+                            <p className='leading-6 h-12 overflow-y-clip font-light'>{e.description}</p>
                             <p className='text-xl leading-10'>Rs. {e.price}<span className='pl-2 text-green-500 text-sm'>50% off</span></p>
                         </div>
                         <div className="flex justify-between px-2 py-2">
@@ -161,9 +240,9 @@ function Products({ setData, data }) {
                     </div>
 
                 ))}
-  
+
             </div>
-            
+
         </>
     )
 }
