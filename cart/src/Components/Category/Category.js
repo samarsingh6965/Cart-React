04db4df13ce7533d7721c0df55d99eb2f8346b1b
Samar Raj <samar.suvaidyam@gmail.com>
2023-05-18 15:12:42 +0530

    this is a message

--- cart/src/Components/Category/Category.js
+++ cart/src/Components/Category/Category.js
@@ -7,9 +7,9 @@ function Category({filter,setFilter}) {
       <div className="w-full px-9 max-sm:px-4 border sticky top-14 bg-white z-10">
         <ul className='flex flex-wrap h-auto items-center'>
             {Categories.map(e=>(
-              <div key={e.id} onClick={()=>setFilter(e.Url)} className={`${filter === e.Url ? "bg-gray-300":""} flex items-center gap-1 border m-2 px-5 py-2 rounded-3xl cursor-pointer`}> 
-                <span className='text-blue-500'>{e.icon}</span>
-                <li className='text-blue-500'>{e.Name}</li>
+              <div key={e.id} onClick={()=>setFilter(e.Url)} className={`${filter === e.Url ? "bg-blue-500 text-white":""} flex duration-300 text-blue-500 items-center gap-1 border m-2 px-5 py-2 rounded-3xl cursor-pointer`}> 
+                <span className='text-lg'>{e.icon}</span>
+                <li>{e.Name}</li>
               </div>
             ))}
         </ul>
